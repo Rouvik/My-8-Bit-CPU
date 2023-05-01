d865189f16bf119a0a29729a9b1f8d9756d21e5c
Rouvik <majirouvik@gmail.com>
2023-05-01 10:06:31 +0530

    Organized my programs in a seperate program file and updated the .gitignore correspondingly

---
+++ Programs/AddAndSub.s
@@ -0,0 +1,11 @@
+; a simple program to add 2 numbers and subtract them and display in my first 8 bit computer
+; by Rouvik Maji 29/04/2023 12:26 AM (IST)
+ldx 126
+ldy 34
+aout
+nop ; keep a bit of delay
+nop
+sout
+nop
+nop
+hlt ; stop program execution
---
+++ Programs/fibo.s
@@ -0,0 +1,9 @@
+; A simple fibonacci program for my 8 bit computer (RCPU-II) to be assembled by rasm
+; written by Rouvik Maji 30th April, 2023 11:27 PM IST
+ldx 1
+ldy 0
+atr ; store sum to R register
+rout ; display term
+rtx ; copy from R to X (inverted order)
+swp ; swap X and Y to settle the terms in order
+jmp 2 ; jump back to instruction 2 (atr)
